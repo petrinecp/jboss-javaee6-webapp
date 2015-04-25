f5dd16e527f75559e313b4fed57fea7e5af5bfa9
Peter Petrinec <petrinecp@gmail.com>
2015-04-25 16:23:02 +0200

    first commit

---
+++ target/generated-sources/annotations/org/jboss/tools/examples/model/BaseEntity_.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.examples.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(BaseEntity.class)
+public abstract class BaseEntity_ {
+
+	public static volatile SingularAttribute<BaseEntity, Object> id;
+
+}
+
---
+++ target/generated-sources/annotations/org/jboss/tools/examples/model/Category_.java
@@ -0,0 +1,14 @@
+package org.jboss.tools.examples.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Category.class)
+public abstract class Category_ extends org.jboss.tools.examples.model.BaseEntity_ {
+
+	public static volatile SingularAttribute<Category, String> title;
+
+}
+
